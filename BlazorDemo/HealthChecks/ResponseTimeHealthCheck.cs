1c53e058cd4d6cc1dcd61c8430062aeccee3cc26
Ana Helena <anahelenarp@hotmail.com>
2020-10-13 18:50:28 -0300

    Added code from Tim Corey's video on Health Checks in .NET Core

---
+++ BlazorDemo/HealthChecks/ResponseTimeHealthCheck.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BlazorDemo.HealthChecks
+{
+    public class ResponseTimeHealthCheck : IHealthCheck
+    {
+        private Random rnd = new Random();
+
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            int responseTimeInMS = rnd.Next(1, 300);
+
+            if (responseTimeInMS < 100)
+            {
+                return Task.FromResult(HealthCheckResult.Healthy($"The response time looks good ({responseTimeInMS}ms)"));
+            }
+            else if (responseTimeInMS < 200)
+            {
+                return Task.FromResult(HealthCheckResult.Degraded($"The response time is a bit slow ({responseTimeInMS})ms"));
+            }
+            else
+            {
+                return Task.FromResult(HealthCheckResult.Unhealthy($"The response time is unacceptable ({responseTimeInMS}ms)"));
+            }
+        }
+    }
+}
---
+++ MVCDemo/Startup.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Hosting;
+
+namespace MVCDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            //Configura Health Checks
+            services.AddHealthChecks()
+                .AddCheck("Foo Service", () =>
+                {
+                    //do the real checks
+                    //then return the real value (healthy or not)
+                    return HealthCheckResult.Healthy("The check of the Foo service worked.");
+                }, new[] { "service" } //tags
+                )
+
+                .AddCheck("Bar Service", () => HealthCheckResult.Degraded("The check of the Bar service did not work well."), new[] { "service" })
+                .AddCheck("Database", () => HealthCheckResult.Healthy("The check of the database worked."), new[] { "database", "sql" });
+
+            //HealthCheckResult.Degraded = there is a problem but the application is still working
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapHealthChecks("/health", new HealthCheckOptions
+                {
+                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                });
+
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
